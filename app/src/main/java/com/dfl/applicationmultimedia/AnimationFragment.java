9afeb87787072e2c8ac8e81d67ad69ca84b4922b
dellylucas <dellylucas@hotmail.com>
2019-12-21 19:34:26 -0500

    animation star

--- app/src/main/java/com/dfl/applicationmultimedia/AnimationFragment.java
+++ app/src/main/java/com/dfl/applicationmultimedia/AnimationFragment.java
@@ -4,17 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AnimationFragment extends Fragment {
+    private ImageView imgStart;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_animation, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FloatingActionButton btnInit = view.findViewById(R.id.btninitanimon);
+        imgStart = view.findViewById(R.id.anim_start);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(view.getContext(), R.anim.animation_start);
+                imgStart.startAnimation(animation);
+            }
+        });
     }
 
     @Override
@@ -25,4 +46,5 @@ public class AnimationFragment extends Fragment {
             mainActivity.updateToolbar("Media Cloud", getString(R.string.init));
 
     }
+
 }
